69e475d48b17e30c52da89ccaafd73e493a13cc7
Bruno Felipe <brunofelipehp8@gmail.com>
2024-04-19 23:39:36 -0300

    Create card price

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,40 @@
+import { FiArrowDownCircle, FiArrowUpCircle } from "react-icons/fi";
+import { PiCurrencyDollarFill } from "react-icons/pi";
+import TablePrice from "./components/TablePrice";
+
 function App() {
   return (
-    <div>
-      <h1 className="flex justify-center mt-20 bg-purple-100 ">Hello word</h1>
+    <div className="flex flex-col justify-center mt-20 items-center gap-28">
+      <div className="flex gap-10 ">
+        <div className="w-72 h-40 rounded-xl bg-purple-600 grid text-white">
+          <div className="flex justify-between m-5">
+            <strong>Entrada</strong>
+            <div>
+              <FiArrowUpCircle size={32} />
+            </div>
+          </div>
+          <span className="ml-5  text-3xl">R$ 3.000,00</span>
+        </div>
+        <div className="w-72 h-40 rounded-xl bg-purple-600 grid text-white ">
+          <div className="flex justify-between m-5">
+            <strong>Saida</strong>
+            <div>
+              <FiArrowDownCircle size={32} />
+            </div>
+          </div>
+          <span className="ml-5  text-3xl">R$ 3.000,00</span>
+        </div>
+        <div className="w-72 h-40 rounded-xl bg-purple-600 grid text-white ">
+          <div className="flex justify-between m-5">
+            <strong>Saldo</strong>
+            <div>
+              <PiCurrencyDollarFill size={32} />
+            </div>
+          </div>
+          <span className="ml-5 text-3xl">R$ 3.000,00</span>
+        </div>
+      </div>
+      <TablePrice />
     </div>
   );
 }
---
+++ src/components/TablePrice.tsx
@@ -0,0 +1,9 @@
+const TablePrice = () => {
+  return (
+    <div>
+      <div className="w-96 h-44 rounded-lg bg-slate-300"></div>
+    </div>
+  );
+};
+
+export default TablePrice;
